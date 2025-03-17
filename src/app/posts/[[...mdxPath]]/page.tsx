109836011b885cab8aadb791089f8df76e7b3d28
BenMix <wubendao@gmail.com>
2025-03-18 03:54:36 +0800

    feat: static generate posts

--- src/app/posts/[[...mdxPath]]/page.tsx
+++ src/app/posts/[[...mdxPath]]/page.tsx
@@ -1,13 +1,28 @@
 import { Wrapper } from "@/components/mdx-wrapper";
+import { getPosts } from "@/lib/get-post";
+import { NextPage } from "next";
 import { importPage } from "nextra/pages";
 
-export default async function Page(props: any) {
-  const params = await props.params;
-  const result = await importPage(params.mdxPath);
-  const { default: MDXContent, toc, metadata } = result;
-  return (
-    <Wrapper toc={toc} metadata={metadata}>
-      <MDXContent {...props} params={params} />
-    </Wrapper>
-  );
+export async function generateStaticParams() {
+  const articles = await getPosts();
+  return articles.map((article) => {
+    const splits = article.route.split("/");
+    return {
+      mdxPath: [encodeURI(splits[splits.length - 1])],
+    };
+  });
 }
+
+const Page: NextPage<{ params: Promise<{ mdxPath: string[] }> }> =
+  async function (props) {
+    const params = await props.params;
+    const result = await importPage(params.mdxPath);
+    const { default: MDXContent, toc, metadata } = result;
+    return (
+      <Wrapper toc={toc} metadata={metadata}>
+        <MDXContent {...props} params={params} />
+      </Wrapper>
+    );
+  };
+
+export default Page;
