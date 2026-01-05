39b91cee6d507898162b9419f0162a0275e30dc4
BenMix <dev@bendao.me>
2026-01-05 14:17:08 +0800

    Enable fumadocs TOC and code highlighting

--- .source/source.config.mjs
+++ .source/source.config.mjs
@@ -1,6 +1,20 @@
 // source.config.ts
 import { defineConfig } from "fumadocs-mdx/config";
-var source_config_default = defineConfig();
+var source_config_default = defineConfig({
+  mdxOptions: {
+    remarkHeadingOptions: {
+      generateToc: true
+    },
+    rehypeCodeOptions: {
+      themes: {
+        light: "github-light-default",
+        dark: "github-dark-default"
+      },
+      lazy: false,
+      defaultColor: false
+    }
+  }
+});
 export {
   source_config_default as default
 };
--- source.config.ts
+++ source.config.ts
@@ -1,3 +1,17 @@
 import { defineConfig } from "fumadocs-mdx/config";
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkHeadingOptions: {
+      generateToc: true,
+    },
+    rehypeCodeOptions: {
+      themes: {
+        light: "github-light-default",
+        dark: "github-dark-default",
+      },
+      lazy: false,
+      defaultColor: false,
+    },
+  },
+});
